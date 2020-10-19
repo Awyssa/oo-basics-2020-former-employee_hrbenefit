673337e60ab30d06cbfa9239ca1acbf852cdd4d9
Michael Ernest <m.hendersun@gmail.com>
2020-10-19 12:47:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,4 +10,10 @@ class shoe
     @condition = condition
   end
 
-  def 
+  def cobble
+    if condition = "old"
+      condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+end
