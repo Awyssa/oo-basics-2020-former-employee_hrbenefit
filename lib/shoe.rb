07eb48fd0d10cd4d1a2bb1ec49a11ab9d80439c8
Michael Ernest <m.hendersun@gmail.com>
2020-10-19 12:45:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,3 +3,11 @@
 class shoe
   attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand, color, size, material, condition)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+
+  def 
